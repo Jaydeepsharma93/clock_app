bd70cc3a947dda7680c06581bee1f72a1f8e4540
jaydeepsharma93 <sharmajaydeep1515@gmail.com>
2024-03-01 12:07:22 +0530

    01/03/2024

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,34 @@
 import 'dart:async';
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Clock_app());
+  runApp(const Clock());
 }
+
+class Clock extends StatefulWidget {
+  const Clock({super.key});
+
+  @override
+  State<Clock> createState() => _ClockState();
+}
+
+class _ClockState extends State<Clock> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Clock_app(),
+        '/stoper': (context) => Stoper()
+      },
+    );
+  }
+}
+
+
+
 
 class Clock_app extends StatefulWidget {
   const Clock_app({super.key});
@@ -19,7 +42,7 @@ DateTime dateTime = DateTime.now();
 class _Clock_appState extends State<Clock_app> {
   @override
   Widget build(BuildContext context) {
-    Timer.periodic(Duration(seconds: 1), (timer) {
+    Timer.periodic(const Duration(seconds: 1), (timer) {
       setState(() {
         dateTime = DateTime.now();
       });
@@ -27,60 +50,130 @@ class _Clock_appState extends State<Clock_app> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        appBar: AppBar(),
         body: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              Text(
-                "${dateTime.hour % 12} : ${dateTime.minute} : ${dateTime.second}",
-                style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+              Column(
+                children: [
+                  Text(
+                    "${dateTime.hour % 12} : ${dateTime.minute}",
+                    style: const TextStyle(
+                        fontSize: 40, fontWeight: FontWeight.bold),
+                  ),
+                ],
               ),
-              Container(
-                  alignment: Alignment.center,
-                  height: 250,
-                  decoration:
-                      BoxDecoration(color: Colors.grey, shape: BoxShape.circle),
-                  child: Stack(
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 50),
+                child: Container(
                     alignment: Alignment.center,
+                    height: 280,
+                    decoration: const BoxDecoration(
+                        color: Color(0xfff9f9f9),
+                        shape: BoxShape.circle,
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black12,
+                              spreadRadius: 10,
+                              blurRadius: 30)
+                        ]),
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Container(
+                          alignment: Alignment.center,
+                          height: 230,
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.white,width: 35),
+                              shape: BoxShape.circle,
+                            // boxShadow: [
+                            //   BoxShadow(
+                              //       color: Colors.black12,
+                              //       spreadRadius: 5,
+                              //       blurRadius: 30)
+                              // ]
+                            ),
+                          child: Container(
+                            height: 160,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle),
+                          ),
+                        ),
+                        // ...List.generate(
+                        //   60,
+                        //   (index) => Transform.rotate(
+                        //     angle: (index * 6 * pi) / 180,
+                        //     child: (index % 5 == 0)
+                        //         ? const VerticalDivider(
+                        //             thickness: 4,
+                        //             endIndent: 240,
+                        //             color: Colors.black)
+                        //         : const VerticalDivider(thickness: 3, endIndent: 245),
+                        //   ),
+                        // ),
+                        const CircleAvatar(
+                          radius: 8,
+                          backgroundColor: Colors.black,
+                        ),
+                        Transform.rotate(
+                          angle: dateTime.second * 6 * pi / 180,
+                          child: const VerticalDivider(
+                            color: Colors.grey,
+                            thickness: 3,
+                            indent: 28,
+                            endIndent: 120,
+                          ),
+                        ),
+                        Transform.rotate(
+                          angle: dateTime.minute * 6 * pi / 180,
+                          child: const VerticalDivider(
+                            color: Colors.black,
+                            thickness: 4,
+                            indent: 35,
+                            endIndent: 122,
+                          ),
+                        ),
+                        Transform.rotate(
+                          angle: (dateTime.hour % 12 + dateTime.minute / 60) *
+                              30 *
+                              pi /
+                              180,
+                          child: const VerticalDivider(
+                            color: Colors.black,
+                            thickness: 5,
+                            indent: 55,
+                            endIndent: 125,
+                          ),
+                        ),
+                      ],
+                    )),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
                     children: [
-                      ...List.generate(
-                          60,
-                          (index) => Transform.rotate(
-                              angle: (index * 6 * pi) / 180,
-                              child: (index%5==0)?Text('1',style:TextStyle(color: Colors.black,fontSize: 30),):VerticalDivider(
-                                  thickness: 3, endIndent: 245))),
-                      CircleAvatar(
-                        radius: 8,
-                        backgroundColor: Colors.black,
-                      ),
-                      Transform.rotate(
-                        angle: dateTime.second * 6 * pi / 180,
-                        child: VerticalDivider(
-                          thickness: 2,
-                          indent: 20,
-                          endIndent: 100,
-                        ),
-                      ),
-                      Transform.rotate(
-                        angle: dateTime.minute * 6 * pi / 180,
-                        child: VerticalDivider(
-                          color: Colors.black,
-                          thickness: 3,
-                          indent: 30,
-                          endIndent: 100,
-                        ),
-                      ),
-                      Transform.rotate(
-                        angle: dateTime.hour * 30 * pi / 180,
-                        child: VerticalDivider(
-                          color: Colors.black,
-                          thickness: 4,
-                          indent: 45,
-                          endIndent: 100,
-                        ),
-                      ),
+                      IconButton(onPressed: (){}, icon: Icon(Icons.access_time_outlined,size: 35,)),
+                      Text("clock",style:TextStyle(fontWeight: FontWeight.bold),)
                     ],
-                  ))
+                  ),
+                  Column(
+                    children: [
+                      IconButton(onPressed: (){}, icon: Icon(Icons.timelapse_outlined,size: 35,)),
+                      Text("timer",style:TextStyle(fontWeight: FontWeight.bold),)
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      IconButton(onPressed: (){
+                        Navigator.pushNamed(context, '/stoper');
+                      }, icon: Icon(Icons.timer_outlined,size: 35,)),
+                      Text("stoper",style:TextStyle(fontWeight: FontWeight.bold),)
+                    ],
+                  )
+                ],
+              )
             ],
           ),
         ),
@@ -88,3 +181,36 @@ class _Clock_appState extends State<Clock_app> {
     );
   }
 }
+
+
+
+class Stoper extends StatefulWidget {
+  const Stoper({super.key});
+
+  @override
+  State<Stoper> createState() => _StoperState();
+}
+
+class _StoperState extends State<Stoper> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Column(
+          children: [
+            Container(
+              child: Column(
+                children: [
+
+                ],
+              ),
+            )
+          ],
+        )
+      ),
+    );
+  }
+}
+
